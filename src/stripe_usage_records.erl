f425f681d92e9ce7654454570e1409f23004f987
Nicolas Martyanoff <nicolas.martyanoff@exograd.com>
2022-03-15 19:01:01 +0100

    add stripe_usage_records:get_summaries/3

--- src/stripe_usage_records.erl
+++ src/stripe_usage_records.erl
@@ -14,12 +14,15 @@
 
 -module(stripe_usage_records).
 
--export([create/3]).
+-export([create/3, get_summaries/3]).
 
--export_type([create_data/0]).
+-export_type([create_data/0, get_summaries_options/0]).
 
 -type create_data() ::
         stripe_client:post_subscription_items_subscription_item_usage_records_request_body().
+
+-type get_summaries_options() ::
+        stripe_client:get_subscription_items_subscription_item_usage_record_summaries_request_query().
 
 -spec create(binary(), create_data(), stripe:client_options()) ->
         stripe:result(stripe_model:usage_record()).
@@ -33,6 +36,40 @@ create(SubscriptionItemId, Data, ClientOptions) ->
   of
     {ok, Record, #{status := S}} when S >= 200, S < 300 ->
       {ok, Record};
+    {ok, #{error := Error}, _} ->
+      {error, {api_error, Error}};
+    {error, Error} ->
+      {error, {client_error, Error}}
+  end.
+
+-spec get_summaries(binary(), get_summaries_options(),
+                    stripe:client_options()) ->
+        stripe:result([stripe_model:usage_record_summary()]).
+get_summaries(SubscriptionItemId, Options, ClientOptions) ->
+  get_summaries(SubscriptionItemId, Options,
+                stripe_utils:client_options(ClientOptions), []).
+
+-spec get_summaries(binary(), get_summaries_options(), stripe_client:options(),
+                    [stripe_model:usage_record_summary()]) ->
+        stripe:result([stripe_model:usage_record_summary()]).
+get_summaries(SubscriptionItemId, Options, ClientOptions, Acc) ->
+  ReqOptions = #{path => #{subscription_item => SubscriptionItemId},
+                 query => Options},
+  case
+    stripe_client:get_subscription_items_subscription_item_usage_record_summaries(
+      ReqOptions, ClientOptions)
+  of
+    {ok, #{data := Summaries, has_more := More}, #{status := S}} when
+        S >= 200, S < 300 ->
+      Acc2 = [Summaries | Acc],
+      case More of
+        true ->
+          #{id := LastId} = lists:last(Summaries),
+          get_summaries(SubscriptionItemId, Options#{starting_after => LastId},
+                        ClientOptions, Acc2);
+        false ->
+          {ok, lists:flatten(lists:reverse(Acc2))}
+      end;
     {ok, #{error := Error}, _} ->
       {error, {api_error, Error}};
     {error, Error} ->
